430693b8e903163a11f0705cae541da58add6ce0
root <oneday.zhang@gmail.com>
2017-06-07 19:30:05 +0800

    update to 1.7
    Signed-off-by: root <oneday.zhang@gmail.com>

---
+++ 1/1.4.py
@@ -0,0 +1,22 @@
+#!/usr/local/bin/python3.6
+from __future__ import division
+print (2+2)
+print (1/2)
+print (1.0/2.0)
+print (1/2.0)
+print (1//2)
+print (1%2)
+print (10%3)
+print (2.75%0.5)
+print (2**3)
+print (-3**2)
+print ((-3)**2)
+print (10000000000000000000000000000000)
+print (12332423432423423432*1342342343241232134+33)
+print (0xAF)
+#python3.x 使用0o开头表示八进制
+print (0o10)
+#变量可以包括字母数字和下划线,不能以数字开头
+x=3
+print (x*2)
+#进行到1.7
