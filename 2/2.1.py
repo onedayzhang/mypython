66c84d9182e1498cc5e6117b014e24b688060688
onedayzhang <oneday.zhang@gmail.com>
2017-06-08 08:23:29 -0700

    2.24
    Signed-off-by: onedayzhang <oneday.zhang@gmail.com>

---
+++ 2/2.1.py
@@ -0,0 +1,40 @@
+#!/usr/local/bin/python3.6
+
+edward=['Edward Gumby',42]
+join=['John Smith',50]
+database=[edward,join]
+print (database)
+
+greeting='Hello'
+print(greeting[0])
+print(greeting[-1])
+print('Hello'[1])
+
+#fourth=input('Year:')[3]
+#print (fourth)
+
+tag='<a href="http://www.python.org">Python web site</a>'
+print (tag[9:30])
+print (tag[32:-4])
+numbers=[1,2,3,4,5,6,7,8,9,10]
+print (numbers[3:6])
+print (numbers[0:1])
+print (numbers[-3:-1])
+print (numbers[-3:])
+print (numbers[:])
+print (numbers[0:10:1])
+print (numbers[0:10:2])
+print (numbers[3:6:3])
+print (numbers[::4])
+print (numbers[10:0:-2])
+print (numbers[0:10:-2])
+print (numbers[::-2])
+print (numbers[5::-2])
+print (numbers[:5:-2])
+#2.2.3
+print ([1,2,3]+[4,5,6])
+#2.2.4
+print('python'*5)
+print ([42]*10)
+sequence=[None]*10
+print (sequence)
