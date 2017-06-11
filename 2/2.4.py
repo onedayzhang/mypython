01f50f8954d63be40ca026d23f1af7a3052c4733
onedayzhang <oneday.zhang@gmail.com>
2017-06-11 07:50:20 -0700

    chapter 2 is ok
    Signed-off-by: onedayzhang <oneday.zhang@gmail.com>

---
+++ 2/2.4.py
@@ -0,0 +1,17 @@
+#!/usr/local/bin/python3.6
+#元组
+x=(1,2,3)
+print (x)
+x=()
+print (x)
+x=(42,)
+print (x)
+
+#tuple函数
+x=tuple([1,2,3])
+print (x)
+x=tuple('abc')
+print (x)
+x=1,2,3
+print (x[1])
+print (x[:])
