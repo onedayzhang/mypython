79a0f9ebecd06ab51699a754de5bfbef07e7233b
onedayzhang <oneday.zhang@gmail.com>
2017-06-16 08:27:15 -0700

    update 3.1
    Signed-off-by: onedayzhang <oneday.zhang@gmail.com>

--- 3/3.1.py
+++ 3/3.1.py
@@ -21,4 +21,9 @@ print ('%10.2f'%pi)
 print ('%.2f'%pi)
 print ('%.5s'%'Guido van Rossum')
 print ('%.*s'%(5,'Guido van Rossum'))
-
+#3.3.3
+print('%010.2f'%pi)
+#减号用来左对齐
+print('%-10.2f'%pi)
+print(('% 5d'%10) +'\n' +('%5d'% -10))
+print(('%+5d'%10) +'\n' +('%5d'% -10))
