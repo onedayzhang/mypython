78b407e8f765cdff95d635e5832a8a048e155f93
onedayzhang <oneday.zhang@gmail.com>
2017-06-21 08:34:37 -0700

    start chapter 5
    Signed-off-by: onedayzhang <oneday.zhang@gmail.com>

---
+++ 5/5.1.py
@@ -0,0 +1,25 @@
+#!/usr/local/bin/python3.6
+import math as footbar
+print(footbar.sqrt(4))
+
+from math import sqrt as footbar
+print(footbar(4))
+
+#5.2
+x,y,z=1,2,3
+print(x,y,z)
+x,y=y,x
+print(x,y,z)
+#5.4
+name=input('What is your name?')
+if name.endswith('Gumby'):
+ print('Hello,Mr.Gumby')
+else:
+ print('Hello, stranger')
+num=int(input('Enter a number:'))
+if num>0:
+    print('positive')
+elif num<0:
+    print('negative')
+else:
+    print('zero')
