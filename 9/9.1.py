3c94141a81999130e18ab2924b2d829d5b17a99b
onedayzhang <oneday.zhang@gmail.com>
2017-07-24 08:05:01 -0700

    update
    Signed-off-by: onedayzhang <oneday.zhang@gmail.com>

--- 9/9.1.py
+++ 9/9.1.py
@@ -115,4 +115,55 @@ r.height=5
 print(r.getSize())
 r.setSize((150,100))
 print(r.width)
+'''
+__metaclass__=type
 
+class MyClass:
+    
+    def smeth():
+        print('This is a static method')
+    meth=staticmethod(smeth)
+    def cmetch(cls):
+        print('This is a class method of',cls)
+    cmetch=classmethod(ceth)
+'''
+
+__metaclass__=type
+
+class MyClass:
+    @staticmethod
+    def smeth():
+        print('This is a static method')
+
+    @classmethod
+    def cmeth(cls):
+        print('This is a class method of',cls)
+MyClass.smeth()
+MyClass.cmeth()
+
+class Fibs:
+    def __init__(self):
+        self.a=0
+        self.b=1
+    def __next__(self):
+        self.a,self.b=self.b,self.a+self.b
+        return self.a
+    def __iter__(self):
+        return self
+
+fibs=Fibs()
+for f in fibs:
+    if f>1000:
+        print(f)
+        break
+
+class TestIterator:
+    value=0
+    def __next__(self):
+        self.value+=1
+        if self.value>10: raise StopIteration
+        return self.value
+    def __iter__(self):
+        return self
+ti=TestIterator()
+print(list(ti))
