2cdafd93d460b3e547219daf3287d029bce0d217
root <oneday.zhang@gmail.com>
2017-07-25 20:02:12 +0800

    update
    Signed-off-by: root <oneday.zhang@gmail.com>

--- 9/9.1.py
+++ 9/9.1.py
@@ -167,3 +167,21 @@ class TestIterator:
         return self
 ti=TestIterator()
 print(list(ti))
+def flatter(nested):
+    for sublist in nested:
+        for element in sublist:
+            yield element
+nested=[[1,2],[3,4],[5]]
+for num in flatter(nested):
+    print (num)
+
+print(list(flatter(nested)))
+
+def flatten(nested):
+    try:
+        for sublist in nested:
+            for element in flatten(sublist):
+                yield element
+    except TypeError:
+        yield nested
+print(list(flatten([[[1],2],3,4,[5,[6,7]],8])))
