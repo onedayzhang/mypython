23b0b7719e8b1ab4f52f8850e0bfbfd755444cbf
onedayzhang <oneday.zhang@gmail.com>
2017-07-29 03:39:56 -0700

    finish capter 10
    Signed-off-by: onedayzhang <oneday.zhang@gmail.com>

--- 10/10.1.py
+++ 10/10.1.py
@@ -19,3 +19,37 @@ print(copy.__file__)
 print(sys.platform)
 import os
 os.system('ifconfig')
+
+print(set(range(10)))
+print(set([0,1,2,3,0,1,2,3,4,6]))
+a=set([1,2,3])
+b=set([2,3,4])
+print(a.union(b))
+print(a|b)
+
+from heapq import *
+from random import shuffle
+data = list(range(10))
+shuffle(data)
+heap=[]
+for n in data:
+    heappush(heap,n)
+print(heap)
+heappush(heap,0.5)
+print(heap)
+print(heappop(heap))
+print(heappop(heap))
+print(heappop(heap))
+print(heap)
+import time
+print(time.asctime())
+print(time.time())
+print(time.localtime())
+print(time.mktime(time.localtime()))
+
+import shelve
+import re
+print(re.__all__)
+
+some_text='alpha,beta....gama delta'
+print(re.split('[,]+',some_text))
